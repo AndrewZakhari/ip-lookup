ef41ece5e2fadcc89ca2f07fcf0a85f702513b9f
deiji <andrewzakhari5@gmail.com>
2023-05-28 13:05:49 +0300

    first touches

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,58 @@
+'use client';
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react';
+
+interface ApiData {
+    ip: String,
+  network: String,
+  version: String,
+  city: String,
+  region: String,
+  region_code: String,
+  country: String,
+  country_name: String,
+  country_code: String,
+  country_code_iso3: String,
+  country_capital: String,
+  country_tld: String,
+  continent_code: String,
+  in_eu: boolean,
+  postal: String,
+  latitude: Number,
+  longitude: Number,
+  timezone: String,
+  utc_offset: String,
+  country_calling_code: String,
+  currency: String,
+  currency_name: String,
+  languages: String,
+  country_area: Number,
+  country_population: Number,
+  asn: String,
+  org: String
+}
+
+const Api = async () => {
+  const [apiData, setApiData] = useState<ApiData>();
+  fetch('https://ipapi.co/8.8.8.8/json/')
+.then((response) => {
+   response.json().then(jsonData => {
+    console.log(jsonData);
+    setApiData(jsonData);
+  });
+})
+.catch(function(error) {
+  console.log(error)
+});
+  console.log(apiData)
+}
 
 export default function Home() {
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   <div>
+    
+   </div> 
   )
 }
